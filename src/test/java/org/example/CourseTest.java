396c806e2629e0c4e68aad29a72b0b0bcb0bdbd6
Otep <josefdrill@gmail.com>
2024-10-29 16:58:24 -0400

    changed build to maven, added javadocs

---
+++ src/test/java/org/example/CourseTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class CourseTest {
+
+    @Test
+    void testEquals() {
+        Course course1 = new Course("Otep", "Otep", "2024", 123);
+        Course course2 = new Course("Otep", "Otep", "2024", 123);
+
+        assert course1.equals(course2);
+    }
+
+    @Test
+    void testNotEquals() {
+        Course course0 = new Course("Otep", "Brotep", "2024", 123);
+        Course course1 = new Course("Otep", "otep", "2024", 123);
+        Course course2 = new Course("Otep", "Otep", "2024", 123);
+
+        assert !course1.equals(course2);
+        assert !course0.equals(course2);
+        assert !course1.equals(course0);
+    }
+
+}
